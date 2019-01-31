d94ebfd9493d680a2dac4e8beb8cd1ec3e497acc
Guillermo <guillermo.delmolino@gmail.com>
2019-01-31 22:36:08 +0100

    Created file and added first code

---
+++ Java/JavaBasics/Introductions.java
@@ -0,0 +1,13 @@
+import java.io.console;
+
+public class Introductions {
+    
+    public static void main(String[] args) {
+        Console console = System.console();
+        String firstName = "Delmo";
+        console.printf("Hello, my name is %s\n", firstName);
+        console.printf("%s is learning how to write Java\n", firstName);
+    }
+}
+
+
